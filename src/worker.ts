eb191fd9be04d943d99f93957d3c04f3412827b6
Doug <douganderson444@gmail.com>
2021-05-02 16:34:58 -0400

    not esvelte version

--- src/worker.ts
+++ src/worker.ts
@@ -4,7 +4,7 @@ import * as rollup from "rollup/dist/es/rollup.browser.js";
 
 // you could use unpkg like the official repl, i thought i'd try out jsdelivr
 const CDN_URL = "https://cdn.jsdelivr.net/npm";
-importScripts(`${CDN_URL}/svelte/compiler.js`); // importScripts method of the WorkerGlobalScope interface synchronously imports one or more scripts into the worker's scope
+importScripts(`${CDN_URL}/svelte/compiler.js`); // @3.35.0 importScripts method of the WorkerGlobalScope interface synchronously imports one or more scripts into the worker's scope
 
 // import the mdsvex worker
 importScripts(`${CDN_URL}/mdsvex/dist/mdsvex.js`)
